1c99d1c06fda859be4cdba45991d50e0786b87ee
Javier Prieto <jprieton@gmail.com>
2015-08-28 16:59:08 -0430

    Ajustes

--- includes/class-subscribers.php
+++ includes/class-subscribers.php
@@ -6,11 +6,11 @@ class Subscribers {
 		global $wpdb;
 		$wpdb instanceof wpdb;
 
-		$charset = !empty($wpdb->charset) ?
+		$charset = !empty( $wpdb->charset ) ?
 						"DEFAULT CHARACTER SET {$wpdb->charset}" :
 						'';
 
-		$collate = !empty($wpdb->collate) ?
+		$collate = !empty( $wpdb->collate ) ?
 						"COLLATE {$wpdb->collate}" :
 						'';
 
@@ -18,44 +18,42 @@ class Subscribers {
 						. "subscriber_id int(11) NOT NULL AUTO_INCREMENT,"
 						. "subscriber_name VARCHAR(255) NULL,"
 						. "subscriber_email VARCHAR(255) NOT NULL,"
+						. "subscriber_source VARCHAR(255) NULL,"
 						. "suscriber_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
 						. "PRIMARY KEY (subscriber_id)"
 						. ") ENGINE=InnoDB {$charset} {$collate} AUTO_INCREMENT=1";
-		$wpdb->query($query);
+		$wpdb->query( $query );
 	}
 
 	public function add_subscriber() {
-		$nonce = filter_input(INPUT_POST, '_wpnonce', FILTER_SANITIZE_STRING);
-		$verify_nonce = (bool) wp_verify_nonce($nonce, 'add_subscriber');
+		$nonce = filter_input( INPUT_POST, '_wpnonce', FILTER_SANITIZE_STRING );
+		$verify_nonce = (bool) wp_verify_nonce( $nonce, 'add_subscriber' );
 
-		$return = 'false';
+		if ( !$verify_nonce ) wp_send_json_error();
 
-		if ($verify_nonce) {
-			$submit = array(
-					'subscriber_email' => filter_input(INPUT_POST, 'subscriber_email', FILTER_SANITIZE_STRING),
-					'subscriber_name'  => filter_input(INPUT_POST, 'subscriber_name', FILTER_SANITIZE_STRING),
-			);
+		$submit = array(
+				'subscriber_email' => filter_input( INPUT_POST, 'subscriber_email', FILTER_SANITIZE_STRING ),
+				'subscriber_name' => filter_input( INPUT_POST, 'subscriber_name', FILTER_SANITIZE_STRING ),
+		);
 
-			if (is_email($submit['subscriber_email'])) {
+		if ( !is_email( $submit['subscriber_email'] ) ) wp_send_json_error();
 
-				global $wpdb;
-				$wpdb instanceof wpdb;
 
-				$result = $wpdb->query("SELECT * FROM {$wpdb->prefix}subscribers WHERE subscriber_email = '{$submit['subscriber_email']}' LIMIT 1");
+		global $wpdb;
+		$wpdb instanceof wpdb;
 
-				if ($result == 0) {
-					$wpdb->insert("{$wpdb->prefix}subscribers", $submit);
-				}
-				$return = 'true';
-			}
+		$result = $wpdb->query( "SELECT * FROM {$wpdb->prefix}subscribers WHERE subscriber_email = '{$submit['subscriber_email']}' LIMIT 1" );
+
+		if ( $result == 0 ) {
+			$wpdb->insert( "{$wpdb->prefix}subscribers", $submit );
 		}
-		header('Content-Type: application/json');
-		die($return);
+
+		wp_send_json_success();
 	}
 
 }
 
 $Subscriber = new Subscribers();
-add_action('wp_ajax_nopriv_add_subscriber', array($Subscriber, 'add_subscriber'));
-add_action('wp_ajax_add_subscriber', array($Subscriber, 'add_subscriber'));
+add_action( 'wp_ajax_nopriv_add_subscriber', array( $Subscriber, 'add_subscriber' ) );
+add_action( 'wp_ajax_add_subscriber', array( $Subscriber, 'add_subscriber' ) );
 
--- jp-subscribers.php
+++ jp-subscribers.php
@@ -3,33 +3,33 @@
  * Plugin Name: JP Subscribe DB
  * Plugin URI:https://github.com/jprieton/jp-subscribe/
  * Description: Ajax suscribe form to DB.
- * Version: 0.2.0
+ * Version: 0.3.0
  * Author: Javier Prieto
  * Author URI: https://github.com/jprieton/
  * License: GPL2
  */
-defined('ABSPATH') or die("No script kiddies please!");
+defined( 'ABSPATH' ) or die( "No script kiddies please!" );
 
 // Updates
-if (is_admin()) {
+if ( is_admin() ) {
 
-	if (!class_exists('BFIGitHubPluginUpdater')) {
+	if ( !class_exists( 'BFIGitHubPluginUpdater' ) ) {
 		require_once __DIR__ . '/updater/BFIGitHubPluginUpdater.php';
 	}
-	if (!class_exists('Parsedown')) {
+	if ( !class_exists( 'Parsedown' ) ) {
 		// We're going to parse the GitHub markdown release notes, include the parser
 		require_once __DIR__ . '/updater/Parsedown.php';
 	}
-	new BFIGitHubPluginUpdater(__FILE__, 'jprieton', 'jp-subscribers');
+	new BFIGitHubPluginUpdater( __FILE__, 'jprieton', 'jp-subscribers' );
 }
 
-define('JPSDB_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('JPSDB_PLUGIN_URI', plugin_dir_url(__FILE__));
+define( 'JPSDB_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'JPSDB_PLUGIN_URI', plugin_dir_url( __FILE__ ) );
 
 include_once JPSDB_PLUGIN_PATH . 'includes/class-subscribers.php';
 
 //Our class extends the WP_List_Table class, so we need to make sure that it's there
-if (!class_exists('WP_List_Table')) {
+if ( !class_exists( 'WP_List_Table' ) ) {
 	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
 
@@ -38,26 +38,26 @@ class Suscriber_List_Table extends WP_List_Table {
 	function get_columns() {
 		$columns = array(
 //			'subscriber_id' => '<input type="checkbox" />',
-				'subscriber_name' => __('Name'),
-				'subscriber_email' => __('Email'),
-				'subscriber_date' => __('Date')
+				'subscriber_name' => __( 'Name' ),
+				'subscriber_email' => __( 'Email' ),
+				'subscriber_date' => __( 'Date' )
 		);
 		return $columns;
 	}
 
-	function column_default($item, $column_name) {
-		switch ($column_name) {
+	function column_default( $item, $column_name ) {
+		switch ( $column_name ) {
 			case 'subscriber_date':
-				return mysql2date(__('Y/m/d'), $item->suscriber_date);
+				return mysql2date( __( 'Y/m/d' ), $item->suscriber_date );
 			case 'subscriber_name':
 			case 'subscriber_email':
 				return $item->{$column_name};
 			default:
-				return print_r($item, true); //Show the whole array for troubleshooting purposes
+				return print_r( $item, true ); //Show the whole array for troubleshooting purposes
 		}
 	}
 
-	function column_subscriber_id($item) {
+	function column_subscriber_id( $item ) {
 		return "<input type='checkbox' name='suscribers[]' id='suscriber_{$item->id}' class='$role' value='{$item->id}' />";
 	}
 
@@ -65,7 +65,7 @@ class Suscriber_List_Table extends WP_List_Table {
 		$columns = $this->get_columns();
 		$hidden = array();
 		$sortable = array();
-		$this->_column_headers = array($columns, $hidden, $sortable);
+		$this->_column_headers = array( $columns, $hidden, $sortable );
 		global $wpdb, $_wp_column_headers;
 		$screen = get_current_screen();
 		$wpdb instanceof wpdb;
@@ -75,9 +75,9 @@ class Suscriber_List_Table extends WP_List_Table {
 
 		/* -- Ordering parameters -- */
 		//Parameters that are going to be used to order the result
-		$orderby = !empty($_GET["orderby"]) ? mysql_real_escape_string($_GET["orderby"]) : 'ASC';
-		$order = !empty($_GET["order"]) ? mysql_real_escape_string($_GET["order"]) : '';
-		if (!empty($orderby) && !empty($order)) {
+		$orderby = !empty( $_GET["orderby"] ) ? mysql_real_escape_string( $_GET["orderby"] ) : 'ASC';
+		$order = !empty( $_GET["order"] ) ? mysql_real_escape_string( $_GET["order"] ) : '';
+		if ( !empty( $orderby ) && !empty( $order ) ) {
 			$query.=' ORDER BY ' . $orderby . ' ' . $order;
 		} else {
 			$query.=' ORDER BY subscriber_id DESC';
@@ -85,29 +85,29 @@ class Suscriber_List_Table extends WP_List_Table {
 
 		/* -- Pagination parameters -- */
 		//Number of elements in your table?
-		$totalitems = $wpdb->query($query); //return the total number of affected rows
+		$totalitems = $wpdb->query( $query ); //return the total number of affected rows
 		//How many to display per page?
 		$perpage = 20;
 		//Which page is this?
-		$paged = !empty($_GET["paged"]) ? mysql_real_escape_string($_GET["paged"]) : '';
+		$paged = !empty( $_GET["paged"] ) ? mysql_real_escape_string( $_GET["paged"] ) : '';
 		//Page Number
-		if (empty($paged) || !is_numeric($paged) || $paged <= 0) {
+		if ( empty( $paged ) || !is_numeric( $paged ) || $paged <= 0 ) {
 			$paged = 1;
 		}
 		//How many pages do we have in total?
-		$totalpages = ceil($totalitems / $perpage);
+		$totalpages = ceil( $totalitems / $perpage );
 		//adjust the query to take pagination into account
-		if (!empty($paged) && !empty($perpage)) {
+		if ( !empty( $paged ) && !empty( $perpage ) ) {
 			$offset = ($paged - 1) * $perpage;
 			$query.=' LIMIT ' . (int) $offset . ',' . (int) $perpage;
 		}
 
 		/* -- Register the pagination -- */
-		$this->set_pagination_args(array(
+		$this->set_pagination_args( array(
 				"total_items" => $totalitems,
 				"total_pages" => $totalpages,
 				"per_page" => $perpage,
-		));
+		) );
 		//The pagination links are automatically built according to those parameters
 
 		/* -- Register the Columns -- */
@@ -115,40 +115,40 @@ class Suscriber_List_Table extends WP_List_Table {
 		$_wp_column_headers[$screen->id] = $columns;
 
 		/* -- Fetch the items -- */
-		$this->items = $wpdb->get_results($query);
+		$this->items = $wpdb->get_results( $query );
 	}
 
-	public function single_row($item) {
+	public function single_row( $item ) {
 		static $row_class = '';
 		$row_class = ( $row_class == '' ? ' class="alternate"' : '' );
 
 		echo '<tr' . $row_class . '>';
-		$this->jp_single_row_columns($item);
+		$this->jp_single_row_columns( $item );
 		echo '</tr>';
 	}
 
-	private function jp_single_row_columns($item) {
+	private function jp_single_row_columns( $item ) {
 		list( $columns, $hidden ) = $this->get_column_info();
 
-		foreach ($columns as $column_name => $column_display_name) {
+		foreach ( $columns as $column_name => $column_display_name ) {
 			$class = "class='$column_name column-$column_name'";
 
 			$style = '';
-			if (in_array($column_name, $hidden)) $style = ' style="display:none;"';
+			if ( in_array( $column_name, $hidden ) ) $style = ' style="display:none;"';
 
 			$attributes = "$class$style";
 
-			if ('cb' == $column_name) {
+			if ( 'cb' == $column_name ) {
 				echo '<th scope="row" class="check-column">';
-				echo $this->column_cb($item);
+				echo $this->column_cb( $item );
 				echo '</th>';
-			} elseif (method_exists($this, 'column_' . $column_name)) {
+			} elseif ( method_exists( $this, 'column_' . $column_name ) ) {
 				echo "<td $attributes>";
-				echo call_user_func(array($this, 'column_' . $column_name), $item);
+				echo call_user_func( array( $this, 'column_' . $column_name ), $item );
 				echo "</td>";
 			} else {
 				echo "<td $attributes>";
-				echo $this->column_default($item, $column_name);
+				echo $this->column_default( $item, $column_name );
 				echo "</td>";
 			}
 		}
@@ -156,18 +156,18 @@ class Suscriber_List_Table extends WP_List_Table {
 
 }
 
-if (is_admin()) {
-	add_action('admin_menu', function () {
-		add_menu_page('JP Subscribers', 'JP Subscribers', 'edit_others_posts', 'jp-subscribers', 'my_plugin_options', 'dashicons-email-alt');
-	});
+if ( is_admin() ) {
+	add_action( 'admin_menu', function () {
+		add_menu_page( 'JP Subscribers', 'JP Subscribers', 'edit_others_posts', 'jp-subscribers', 'my_plugin_options', 'dashicons-email-alt' );
+	} );
 }
 
-add_action('wp_ajax_jp_subscribers_csv_export', function () {
+add_action( 'wp_ajax_jp_subscribers_csv_export', function () {
 
 	global $wpdb;
 	$wpdb instanceof wpdb;
 
-	$subscribers = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}subscribers");
+	$subscribers = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}subscribers" );
 
 	$fields = array(
 			'subscriber_id',
@@ -176,31 +176,31 @@ add_action('wp_ajax_jp_subscribers_csv_export', function () {
 			'suscriber_date',
 	);
 
-	function clean_string_csv($str) {
-		return '"' . str_replace(',', '\,', $str) . '"';
+	function clean_string_csv( $str ) {
+		return '"' . str_replace( ',', '\,', $str ) . '"';
 	}
 
-	header('Content-Description: File Transfer');
-	header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename=export.csv');
-	header('Content-Transfer-Encoding: binary');
-	header('Connection: Keep-Alive');
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-	header('Pragma: public');
+	header( 'Content-Description: File Transfer' );
+	header( 'Content-Type: application/octet-stream' );
+	header( 'Content-Disposition: attachment; filename=export.csv' );
+	header( 'Content-Transfer-Encoding: binary' );
+	header( 'Connection: Keep-Alive' );
+	header( 'Expires: 0' );
+	header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+	header( 'Pragma: public' );
 
-	$_subscriber = array_map('clean_string_csv', (array) $fields);
-	echo implode(';', $_subscriber);
+	$_subscriber = array_map( 'clean_string_csv', (array) $fields );
+	echo implode( ';', $_subscriber );
 	echo "\n";
 
-	foreach ($subscribers as $item) {
+	foreach ( $subscribers as $item ) {
 		$subscriber = array();
-		foreach ($fields as $field) {
+		foreach ( $fields as $field ) {
 			$_item = (array) $item;
 			$subscriber[] = $_item[$field];
 		}
-		$_subscriber = array_map('clean_string_csv', (array) $subscriber);
-		echo implode(';', $_subscriber);
+		$_subscriber = array_map( 'clean_string_csv', (array) $subscriber );
+		echo implode( ';', $_subscriber );
 		echo "\n";
 	}
 	die;
@@ -208,13 +208,13 @@ add_action('wp_ajax_jp_subscribers_csv_export', function () {
 );
 
 function my_plugin_options() {
-	if (!current_user_can('manage_options')) {
-		wp_die(__('You do not have sufficient permissions to access this page.'));
+	if ( !current_user_can( 'manage_options' ) ) {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	?>
 	<div class="wrap">
 		<h2>Suscriptores</h2>
-		<p><a href="<?php echo admin_url('admin-ajax.php?action=jp_subscribers_csv_export') ?>">Descargar csv</a></p>
+		<p><a href="<?php echo admin_url( 'admin-ajax.php?action=jp_subscribers_csv_export' ) ?>">Descargar csv</a></p>
 		<?php
 		//Prepare Table of elements
 		$wp_list_table = new Suscriber_List_Table();
